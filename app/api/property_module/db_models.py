0a698e5a12e37f105a8db0e4f237ed0fb549815b
gabriel-lunesu <g.lunesu@outlook.com>
2024-05-08 13:00:27 +0200

    Update db_models.py

--- app/api/property_module/db_models.py
+++ app/api/property_module/db_models.py
@@ -4,23 +4,7 @@ from typing import List, Optional
 from pydantic import BaseModel, EmailStr, Field
 
 
-# class PropertySchema(BaseModel):
-#     fullname: str = Field(...)
-#     email: EmailStr = Field(...)
-#     course_of_study: str = Field(...)
-#     year: int = Field(..., gt=0, lt=9)
-#     gpa: float = Field(..., le=4.0)
 
-#     class Config:
-#         schema_extra = {
-#             "example": {
-#                 "fullname": "John Doe",
-#                 "email": "jdoe@x.edu.ng",
-#                 "course_of_study": "Water resources engineering",
-#                 "year": 2,
-#                 "gpa": "3.0",
-#             }
-#         }
 
 class DetailItem(BaseModel):
     label: str
